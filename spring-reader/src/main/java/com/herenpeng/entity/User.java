5bfbcbed6c2e8b546c82372b0861ca8e2ad05782
herenpeng <2519832582@qq.com>
2021-02-06 20:22:10 +0800

    阅读Spring源码的第一天

---
+++ spring-reader/src/main/java/com/herenpeng/entity/User.java
@@ -0,0 +1,28 @@
+package com.herenpeng.entity;
+
+/**
+ * @author herenpeng
+ * @since 2021-02-06 19:51
+ */
+public class User {
+
+	private String username;
+
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ spring-reader/src/main/java/com/herenpeng/read/SpringReader.java
@@ -0,0 +1,20 @@
+package com.herenpeng.read;
+
+import com.herenpeng.entity.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author herenpeng
+ * @since 2021-02-06 19:48
+ */
+public class SpringReader {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		User user = applicationContext.getBean("user", User.class);
+		user.setUsername("I am a Spring reader");
+		System.out.println(user.getUsername());
+	}
+
+}
